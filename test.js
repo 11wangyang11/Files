a3c3eb9e7fffc3c0583aa88e68942fa90c38979e
汪洋 <wangyang@wangyangdeMacBook-Pro.local>
2025-04-17 22:22:10 +0800

    feat: 排序

--- test.js
+++ test.js
@@ -22,17 +22,21 @@ head.next.next.next = new ListNode(3);
 // s = "3[a2[c]]"
 // accaccacc
 
-function bubbleSort(arr) {
-    let n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-      for (let j = 0; j < n - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // 交换相邻元素
-        }
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let current = arr[i];
+      let j = i - 1;
+      while (j >= 0 && arr[j] > current) {
+        arr[j + 1] = arr[j]; // 向后移动元素
+        j--;
+        console.log('11', arr, i, j)
       }
+      console.log('22', arr, i, j)
+      arr[j + 1] = current; // 插入正确位置
     }
     return arr;
   }
 
+const res = insertionSort([3,1,4,0]);
 console.log(res);
 
