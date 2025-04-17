8546e7771e61e9742cdb3003c6cd7e1efd375d09
yangwang7 <yangwang7@trip.com>
2025-04-17 20:12:41 +0800

    feat: 排序

--- test.js
+++ test.js
@@ -9,34 +9,30 @@ const node3 = new ListNode(3);
 const node2 = new ListNode(2, node3);
 const node1 = new ListNode(1, node2);
 
-let head = new ListNode(5);
-head.next = new ListNode(3);
-head.next.next = new ListNode(9);
+let head = new ListNode(4);
+head.next = new ListNode(2);
+head.next.next = new ListNode(1);
+head.next.next.next = new ListNode(3);
 
-539
-123
+// 2314502
+// 210
 
-function addTwoList(h1, h2) {
-    let head = new ListNode(0);
-    let cur = head;
-    let carry = 0;
-    while(h1 || h2) {
-        let val1 = h1 ? h1.val : 0;
-        let val2 = h2 ? h2.val : 0;
-        let sum = (val1 + val2 + carry);
-        carry = Math.floor(sum / 10); // 取整
-        cur.next = new ListNode(sum % 10);
-        cur = cur.next;
-        h1 = h1.next;
-        h2 = h2.next;
+// s = "3[a]2[bc]"
+// aaabcbc
+// s = "3[a2[c]]"
+// accaccacc
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // 交换相邻元素
+        }
+      }
     }
-    if (carry) {
-        cur.next = new ListNode(carry);
-    }
-    return head.next;
-}
-
-const res = addTwoList(node1, head);
+    return arr;
+  }
 
 console.log(res);
 
