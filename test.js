2500430bb92433ab351b68a7d2a0add4096fdd38
汪洋 <wangyang@wangyangdeMacBook-Pro.local>
2025-04-19 11:52:19 +0800

    feat: 二叉树

--- test.js
+++ test.js
@@ -14,29 +14,60 @@ head.next = new ListNode(2);
 head.next.next = new ListNode(1);
 head.next.next.next = new ListNode(3);
 
-// 2314502
-// 210
-
-// s = "3[a]2[bc]"
-// aaabcbc
-// s = "3[a2[c]]"
-// accaccacc
-
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-      let current = arr[i];
-      let j = i - 1;
-      while (j >= 0 && arr[j] > current) {
-        arr[j + 1] = arr[j]; // 向后移动元素
-        j--;
-        console.log('11', arr, i, j)
-      }
-      console.log('22', arr, i, j)
-      arr[j + 1] = current; // 插入正确位置
+class TreeNode {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
-    return arr;
-  }
+}
 
-const res = insertionSort([3,1,4,0]);
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+/**
+ *          1
+ *       2     3
+ *     4  5   6  7
+ */
+// [1] 1
+// [3][2]  2
+// [3][5][4] 4-5
+// [3]
+// [7][6]    3
+//           6-7
+
+/**
+ * 前序遍历： 1-2-3-4-5-6-7-8
+ */
+
+function leverOrder(root) {
+    if(!root) {
+        return [];
+    }
+    let queue = [root];
+    let result = [];
+    while(queue.length) {
+        const length = queue.length;
+        for(let i=0;i<length;i++) {
+            const node = queue.shift();
+            // let level = []; // 也可以每层放进一个数组中
+            // level.push(node.val);
+            result.push(node.val);
+            if(node.left) {
+                queue.push(node.left);
+            }
+            if(node.right){
+                queue.push(node.right);
+            }
+        }
+    }
+    return result;
+}
+const res = leverOrder(root);
+
 console.log(res);
-
