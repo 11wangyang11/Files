96b86fe82d5df325a32d55fdeba15315ea5d8d55
汪洋 <wangyang@wangyangdeMacBook-Pro.local>
2025-06-08 15:35:09 +0800

    feat: 动态规划

--- test2.js
+++ test2.js
@@ -1,22 +1,52 @@
-var findMin = function(nums) {
-    let start=0,end = nums.length-1;
-    while(start<=end) {
-        const mid = Math.floor((start+end)/2);
-        // 最小值在中间
-        if(nums[start] > nums[end]) {
-            console.log('111', nums[mid], nums[start], nums[end])
-            if(nums[mid] >= nums[start]) { // mid在最小值(旋转位置)的左边，
-                start = mid+1;
-            } else if (mid >0 && nums[mid] > nums[mid-1]) { // mid在最小值右边
-                end = mid-1;
-            } else {
-                return nums[mid]; // mid就是最小值
-            }
-        } else {
-            return nums[start];
-        }
+var topKFrequent = function(nums, k) {
+    const countMap = new Map();
+    // 构建一个出现次数的哈希表
+    for(const num of nums) {
+        const cur = countMap.get(num);
+        countMap.set(num, (cur || 0)+1);
     }
+    console.log('000', countMap)
+    let minHeap = [], index = 0;
+    // 找出哈希表第k大的元素
+    for(const [key, value] of countMap) {
+        if (index < k) {
+            minHeap.push({key, value});
+        } else {
+            if (index === k){
+                // 构建长度为k的小顶堆(包含元素和频率，以频率作为判断依据)
+                for(let i = Math.floor((k-1)/2); i>=0;i--){
+                    buildHeap(minHeap, k, i);
+                }
+            }
+            // 遍历剩余元素，存储频率前k的元素
+            if(minHeap[0].value < value) {
+                minHeap[0] = {key, value};
+                buildHeap(minHeap, k, 0); 
+            }
+        }
+        index++;
+        console.log('111', minHeap)
+    }
+    // 输出前k高的所有元素
+    let res = [];
+    for(const {key, value} of minHeap) {
+        res.push(key);
+    }
+    return res;
 };
-nums = [3,1,2]
-const res = findMin(nums);
+
+function buildHeap(nums, n, i) {
+    let min = i;
+    let left = i*2+1;
+    let right = i*2+2;
+    // 找出左右节点的最小值
+    if(left<n && nums[left].value < nums[min].value) min = left;
+    if(right<n && nums[right].value < nums[min].value) min = right;
+    if(min !==i) {
+        [nums[i], nums[min]] = [nums[min], nums[i]];
+        buildHeap(nums, n, min); // 递归到最底层，处理由交换引起的下层结构的变化
+    }
+}
+nums = [1,1,1,2,2,3], 
+res = topKFrequent(nums, 2);
 console.log(res);
